3af4e2a0138e521f573d215f082a6f219c62ce6f
MichaelGese202 <proafinder@gmail.com>
2025-03-30 13:44:13 -0500

    Update data_proyect.js

--- data_proyect.js
+++ data_proyect.js
@@ -1,5 +1,29 @@
 const airdrops = [
                                     {
+                id: 25,
+                title: " Mage Labs",
+                type: "gaming",
+                description: "Complete the tasks to qualify for potential  airdrops and rewards ",
+                launchDate: " ",
+                status: "Confirmed", 
+                image: "https://pbs.twimg.com/profile_images/1814556272127320065/PWwjbKUu_400x400.jpg ",
+                referenceImage: " https://dropsearn.fra1.cdn.digitaloceanspaces.com/media/projects/screenshots/Mage-Labs-Tokenomics.webp",
+                webUrl: "https://t.me/VersusTrainerBot?start=7455236642 ",
+                docsUrl: " https://docsend.com/view/ye23ubfi64hjna8s ",
+                xUrl: " https://x.com/Mage_Labs",
+                badges: ["Web","Docs","X"],
+                info: " Mage Labs is a game development studio founded in 2021 which seeks to develop the new gold standard of web3 gaming by marrying advanced AI technology and next-gen play to earn featuring stable coin earnings.",
+                funds:"$ 180 K ",
+                code: " ",
+                link: " https://t.me/VersusTrainerBot?start=7455236642",
+                participationSteps: [
+                   { step: "Log in with Telegram in the game and earn points.", icon: "fa-paper-plane" },
+                   { step: "Complete all available tasks.", icon: "fa-tasks" },
+                   { step: "Share with friends to earn more points.", icon: "fa-users" }
+                ]
+ 
+            },
+                                    {
                 id: 24 ,
                 title: " CRYPLEX ",
                 type: "depin",
