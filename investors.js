4b0dec94d0aaff8b5420fc32d9371a1468a90c29
MichaelGese202 <proafinder@gmail.com>
2025-04-03 17:39:32 -0500

    Update investors.js

--- investors.js
+++ investors.js
@@ -1,26 +1,26 @@
 // investors.js
 const investors = {
-    "Binance Labs": {
-        url: "https://labs.binance.com",
-        logo: "https://example.com/binance-labs-logo.png"
+    "ChainLink": {
+        url: "https://chain.link/",
+        logo: "https://dropsearn.fra1.cdn.digitaloceanspaces.com/media/cache/e6/b5/e6b515e9edd0113bd6a5f363c5d0e85f.webp"
     },
-    "Coinbase Ventures": {
-        url: "https://www.coinbase.com/ventures",
-        logo: "https://example.com/coinbase-ventures-logo.png"
+    "Immutable": {
+        url: "https://www.immutable.com/",
+        logo: "https://cdn.prod.website-files.com/646557ee455c3e16e4a9bcb3/646557ee455c3e16e4a9bcbe_immutable-logo.svg"
     },
-    "a16z Crypto": {
-        url: "https://a16z.com/crypto/",
-        logo: "https://example.com/a16z-logo.png"
+    "Multicoin Capital": {
+        url: "https://multicoin.capital/",
+        logo: "https://dropsearn.fra1.cdn.digitaloceanspaces.com/media/cache/3c/58/3c58e7002ed133ff3fd25c234b0e2f52.webp"
     },
-    "Paradigm": {
-        url: "https://www.paradigm.xyz/",
-        logo: "https://example.com/paradigm-logo.png"
+    "Pantera Capital": {
+        url: "https://panteracapital.com/",
+        logo: "https://dropsearn.fra1.cdn.digitaloceanspaces.com/media/cache/13/28/1328238f459798f055c80f7a871fbbf8.webp"
     },
-    "Polychain Capital": {
-        url: "https://polychain.capital/",
-        logo: "https://example.com/polychain-logo.png"
+    "HongShan (ex-Sequoia China)": {
+        url: "https://redbeard.ventures/",
+        logo: "https://img.cryptorank.io/funds/150x150.red_beard_ventures1664361192749.png"
     },
-    "Animoca Brands": {
+    "Red Beard Ventures": {
         url: "https://www.animocabrands.com/",
         logo: "https://example.com/animoca-logo.png"
     },
