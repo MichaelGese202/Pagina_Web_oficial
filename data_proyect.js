0188d34295742a8219de167da8aa450d034225c0
MichaelGese202 <proafinder@gmail.com>
2025-04-03 17:43:28 -0500

    Update data_proyect.js

--- data_proyect.js
+++ data_proyect.js
@@ -16,9 +16,8 @@ const airdrops = [
                 info: "",
                 funds:"$ 1.05 M",
                 link: "https://dashboard.3dos.io/register?ref_code=caf5d1",
-                investors: [
-                  {name: "Red Beard Ventures ", url:"https://redbeard.ventures/", logo: "https://img.cryptorank.io/funds/150x150.red_beard_ventures1664361192749.png"}
-                 ]
+                investorNames: ["Red Beard Ventures"],
+                
                 
             },
                 {
@@ -37,11 +36,7 @@ const airdrops = [
                 badges: ["Web", "X"],
                 info: "Complete the tasks to qualify for potential Gradient Network airdrops and rewards",
                 funds: "$0",
-                investors: [
-                  {name: " Multicoin Capital", url:"https://multicoin.capital/", logo: "https://dropsearn.fra1.cdn.digitaloceanspaces.com/media/cache/3c/58/3c58e7002ed133ff3fd25c234b0e2f52.webp"},
-                  {name: "Pantera Capital", url: "https://panteracapital.com/ ", logo: " https://dropsearn.fra1.cdn.digitaloceanspaces.com/media/cache/13/28/1328238f459798f055c80f7a871fbbf8.webp"},
-                  {name: "HongShan (ex-Sequoia China)", url:"https://www.hsgcap.com/", logo:"https://dropsearn.fra1.cdn.digitaloceanspaces.com/media/cache/68/a3/68a3d85a127e63c2aae68d76417c1a2b.webp"}
-                ],
+                investorNames: ["Multicoin Capital","Pantera Capital","HongShan (ex-Sequoia China)"],
                 link: " https://app.gradient.network/signup?code=SCCMOG"
             },
                                       {
@@ -64,10 +59,7 @@ const airdrops = [
                 badges: ["Web","Docs","X"],
                 info: " Mage Labs is a game development studio founded in 2021 which seeks to develop the new gold standard of web3 gaming by marrying advanced AI technology and next-gen play to earn featuring stable coin earnings.",
                 funds:"$180 K",
-                investors: [
-                  {name: "ChainLink", url:"https://chain.link/", logo: "https://dropsearn.fra1.cdn.digitaloceanspaces.com/media/cache/e6/b5/e6b515e9edd0113bd6a5f363c5d0e85f.webp"},
-                  {name: "Immutable", url: "https://www.immutable.com/", logo: "https://cdn.prod.website-files.com/646557ee455c3e16e4a9bcb3/646557ee455c3e16e4a9bcbe_immutable-logo.svg"}
-                ],
+                investorNames: ["ChainLink","Immutable"],
                 code: " ",
                 link: " https://t.me/VersusTrainerBot?start=7455236642",
                 participationSteps: [
