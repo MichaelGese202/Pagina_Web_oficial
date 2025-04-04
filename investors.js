ddfb8d66023844a6a7318e5673576d0eacd2c73e
MichaelGese202 <proafinder@gmail.com>
2025-04-03 20:54:32 -0500

    Update investors.js

--- investors.js
+++ investors.js
@@ -1,37 +1,34 @@
 // investors.js
 const investorsData = {
     "ChainLink": {
-        name: "ChainLink",
         url: "https://chain.link/",
         logo: "https://dropsearn.fra1.cdn.digitaloceanspaces.com/media/cache/e6/b5/e6b515e9edd0113bd6a5f363c5d0e85f.webp"
     },
     "Immutable": {
-        name: "Immutable",
         url: "https://www.immutable.com/",
         logo: "https://cdn.prod.website-files.com/646557ee455c3e16e4a9bcb3/646557ee455c3e16e4a9bcbe_immutable-logo.svg"
     },
     "Multicoin Capital": {
-        name: "Multicoin Capital",
         url: "https://multicoin.capital/",
         logo: "https://dropsearn.fra1.cdn.digitaloceanspaces.com/media/cache/3c/58/3c58e7002ed133ff3fd25c234b0e2f52.webp"
     },
     "Pantera Capital": {
-        name: "Pantera Capital",
+         
         url: "https://panteracapital.com/",
         logo: "https://dropsearn.fra1.cdn.digitaloceanspaces.com/media/cache/13/28/1328238f459798f055c80f7a871fbbf8.webp"
     },
     "HongShan (ex-Sequoia China)": {
-        name: "HongShan (ex-Sequoia China)",
+        
         url: "https://redbeard.ventures/",
         logo: "https://img.cryptorank.io/funds/150x150.red_beard_ventures1664361192749.png"
     },
     "Red Beard Ventures": {
-        name: "Red Beard Ventures",
+         
         url: "https://www.animocabrands.com/",
         logo: "https://example.com/animoca-logo.png"
     },
     "Blockchain Founders Fund": {
-        name: "Blockchain Founders Fund",
+        
         url: "https://blockchainff.com/",
         logo: "https://img.cryptorank.io/funds/150x150.blockchain_founders_fund1666619105678.png"
     }
